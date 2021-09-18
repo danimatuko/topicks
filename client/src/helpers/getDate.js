0d7c4064c5a3e8018d6b20717bc76bd5c137bc37
Dani Matuko <matuko305@gmail.com>
2021-09-18 12:18:45 +0300

    .fixing gitignore

--- client/src/helpers/getDate.js
+++
@@ -1,9 +0,0 @@
-export const getDate = () => {
-	let date = new Date();
-	let day = date.getDate();
-	let month = date.getMonth() + 1;
-	let year = date.getFullYear();
-
-	let fullDate = `${day}.${month}.${year}`;
-	return fullDate;
-};
--- server/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateToken = (id, first_name, last_name, role) => {
-	const token = jwt.sign({ id, first_name, last_name, role }, process.env.JWT_SECRET, {
-		expiresIn: "1h"
-	});
-	return token;
-};
