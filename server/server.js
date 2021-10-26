cfe886baf26644a0be46c04f684239b998f867bc
Dani Matuko <matuko305@gmail.com>
2021-10-26 19:24:56 +0300

    add public folder to path

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/client/build")));
 	// Express will serve up index.html file if it doesn't recognize the route
 	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+		res.sendFile(path.resolve(__dirname, "client", "build", "public", "index.html"))
 	);
 }
 
